9a889369aa25b727d12996f73df0858f8df91e5a
void050905 <1823066456@qq.com>
2024-10-19 19:52:00 +0800

    update

--- demo.py
+++ demo.py
@@ -1,23 +1,26 @@
 import gradio as gr
+import numpy as np
 import torch
-from torchvision import transforms
-import requests
-from PIL import Image
+from model import MyLeNet5
 
-model = torch.hub.load('D:/LeNet/save_model/best_model.pth', 'resnet18', pretrained=True).eval()
+# 实例化模型
+digit_model = MyLeNet5()
 
-# Download human-readable labels for ImageNet.
-response = requests.get("https://git.io/JJkYN")
-labels = response.text.split("\n")
+# 安全地加载模型权重
+weights = torch.load('D:/LeNet/save_model/best_model.pth', weights_only=True)
+digit_model.load_state_dict(weights)  # 加载权重
+digit_model.eval()  # 设置模型为评估模式
 
-def predict(inp):
-  inp = Image.fromarray(inp.astype('uint8'), 'RGB')
-  inp = inp.resize((24, 24))
-  inp = transforms.ToTensor()(inp).unsqueeze(0)
-  with torch.no_grad():
-    prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-  return {labels[i]: float(prediction[i]) for i in range(1000)}
+def classify_image(img):
+    img_3d = img.reshape(1, 1, 28, 28)  # 处理输入格式
+    im_resize = img_3d / 255.0
+    im_tensor = torch.tensor(im_resize, dtype=torch.float32)  # 转换为Tensor
+    with torch.no_grad():  # 禁用梯度计算
+        prediction = digit_model(im_tensor)  # 进行预测
+    pred = torch.argmax(prediction, dim=1).item()  # 获取预测结果
+    return pred
 
-inputs = gr.Image()
-outputs = gr.Label(num_top_classes=3)
-gr.Interface(fn=predict, inputs=inputs, outputs=outputs).launch()
+# 使用gr.Label而不是gr.outputs.Label
+label = gr.Label(num_top_classes=3)
+interface = gr.Interface(fn=classify_image, inputs="sketchpad", outputs=label)
+interface.launch(share=True)
